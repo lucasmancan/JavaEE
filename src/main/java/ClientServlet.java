32efc5894c72babc85e4314733b7e179ba59988e
lucasmancan <lucasfmancan@hotmail.com>
2018-06-25 22:01:45 -0300

    First Commit

---
+++ src/main/java/ClientServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/cliente","/clienteController" })
+public class ClientServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    //System.out.println("GET!");
+
+        resp.getWriter().print("Chamou pelo metodo GET!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       // System.out.println("POST!");
+
+        resp.getWriter().print("Chamou pelo metodo POST!");
+    }
+}
