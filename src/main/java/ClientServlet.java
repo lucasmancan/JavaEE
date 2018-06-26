94b79b61f7226671183de3670d34408ba4bfc481
lucasmancan <lucasfmancan@hotmail.com>
2018-06-25 23:08:09 -0300

    Commit 02

--- src/main/java/ClientServlet.java
+++ src/main/java/ClientServlet.java
@@ -1,3 +1,4 @@
+import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,17 +9,30 @@ import java.io.IOException;
 @WebServlet(urlPatterns = {"/cliente","/clienteController" })
 public class ClientServlet extends HttpServlet {
 
+
+
+    @Override
+    public void destroy() {
+        System.out.println("Destruindo Servlet");
+        super.destroy();
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       // resp.getWriter().print("Chamando service...");
+        super.service(req, resp);
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     //System.out.println("GET!");
-
         resp.getWriter().print("Chamou pelo metodo GET!");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
        // System.out.println("POST!");
-
-        resp.getWriter().print("Chamou pelo metodo POST!");
+        String email = req.getParameter("email");
+        resp.getWriter().print("Chamou pelo metodo POST no email : "+ email);
     }
 }
